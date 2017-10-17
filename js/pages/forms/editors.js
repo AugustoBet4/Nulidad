cd1b21a2ef6495f2b07c3d94fd31c5e0b3acb4ef
AugustoBet4 <augusto.bet4@gmail.com>
2017-10-16 23:54:15 -0400

    Actualizacion introduccion

--- js/pages/forms/editors.js
+++ js/pages/forms/editors.js
@@ -9,12 +9,12 @@ $(function () {
         height: 300,
         plugins: [
             'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'searchreplace visualblocks visualchars code fullscreen',
             'insertdatetime media nonbreaking save table contextmenu directionality',
             'emoticons template paste textcolor colorpicker textpattern imagetools'
         ],
-        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        toolbar1: 'insertfile undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor',
         image_advtab: true
     });
     tinymce.suffix = ".min";
