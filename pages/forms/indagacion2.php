87e6f481490c4ba82836a7f343ef1eb14213155d
AugustoBet4 <augusto.bet4@gmail.com>
2017-11-14 03:01:58 -0400

    indagacion - cambions minimos

--- pages/forms/indagacion2.php
+++ pages/forms/indagacion2.php
@@ -49,20 +49,6 @@
   <!-- Overlay For Sidebars -->
   <div class="overlay"></div>
   <!-- #END# Overlay For Sidebars -->
-<<<<<<< HEAD
-  <!-- Search Bar -->
-  <!-- <div class="search-bar">
-    <div class="search-icon">
-      <i class="material-icons">search</i>
-    </div>
-    <input type="text" placeholder="Buscar caso...">
-    <div class="close-search">
-      <i class="material-icons">close</i>
-    </div>
-  </div> -->
-  <!-- #END# Search Bar -->
-=======
->>>>>>> f4a5da6270fd9605f4c6ceaca81004fdf24f24f9
   <!-- Top Bar -->
   <nav class="navbar">
     <div class="container-fluid">
@@ -71,16 +57,6 @@
         <a href="javascript:void(0);" class="bars"></a>
         <a class="navbar-brand" href="../../index.php">SEMINARIO</a>
       </div>
-<<<<<<< HEAD
-      <div class="collapse navbar-collapse" id="navbar-collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <!-- Call Search -->
-          <!-- <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li> -->
-          <!-- #END# Call Search -->
-        </ul>
-      </div>
-=======
->>>>>>> f4a5da6270fd9605f4c6ceaca81004fdf24f24f9
     </div>
   </nav>
   <!-- #Top Bar -->
