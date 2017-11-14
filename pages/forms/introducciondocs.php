91ea805a900c5fa9f5055783e570b73e531a7c0b
Adriana Centellas <adrianacentellas98@gmail.com>
2017-11-14 00:23:50 -0400

    introduccion - menu documentos arreglado

--- pages/forms/introducciondocs.php
+++ pages/forms/introducciondocs.php
@@ -152,36 +152,67 @@
                               <h2>Registro de la demanda</h2>
                               <section>
                                   <p>
-                                      Registro del escrito de la demanda
+                                      <a href="registrodemanda.php">Realizar registro del escrito de la demanda.</a>
                                   </p>
                               </section>
                               <h2>Aceptación demanda</h2>
                               <section>
                                   <p>
-                                    Decreto de admisión de la demanda y formula de dudas
+                                    <a href="aceptaciondemanda.php">Decreto de admisión de la demanda y formula de dudas.</a>
                                   </p>
                               </section>
-                              <h2>Third Step</h2>
+                              <h2>Citación de la demanda</h2>
                               <section>
                                   <p>
-                                      Morbi ornare tellus at elit ultrices id dignissim lorem elementum. Sed eget nisl at justo
-                                      condimentum dapibus. Fusce eros justo, pellentesque non euismod ac, rutrum sed quam.
-                                      Ut non mi tortor. Vestibulum eleifend varius ullamcorper. Aliquam erat volutpat.
-                                      Donec diam massa, porta vel dictum sit amet, iaculis ac massa. Sed elementum dui
-                                      commodo lectus sollicitudin in auctor mauris venenatis.
+                                    <a href="citaciondemanda.php">Cedula de citación al demandado.</a>
                                   </p>
-                              </section>
-                              <h2>Forth Step</h2>
-                              <section>
-                                  <p>
-                                      Quisque at sem turpis, id sagittis diam. Suspendisse malesuada eros posuere mauris vehicula
-                                      vulputate. Aliquam sed sem tortor. Quisque sed felis ut mauris feugiat iaculis nec
-                                      ac lectus. Sed consequat vestibulum purus, imperdiet varius est pellentesque vitae.
-                                      Suspendisse consequat cursus eros, vitae tempus enim euismod non. Nullam ut commodo
-                                      tortor.
-                                  </p>
-                              </section>
+                                </section>
+                                  <h2>Comparecencia</h2>
+                                  <section>
+                                    <p>
+                                      <a href="comparecencia.php">Comparecencia de demandante.</a>
+                                    </p>
+                                  </section>
+                                  <h2>Decreto citación por edicto</h2>
+                                  <section>
+                                      <p>
+                                          <a href="decretoedicto.php">Decreto del vicario judicial disponiendo citar por edicto.</a>
+                                      </p>
+                                  </section>
+                                  <h2>Edicto</h2>
+                                  <section>
+                                      <p>
+                                          <a href="edictopublicar.php">Edicto a publicar.</a>
+                                      </p>
+                                  </section>
+                                  <h2>Citación por exhorto</h2>
+                                  <section>
+                                      <p>
+                                          <a href="citacionexhorto.php">Emplazamiento del demandado a través de exhorto a otro tribunal.</a>
+                                      </p>
+                                  </section>
+                                  <h2>Respuesta demandado</h2>
+                                  <section>
+                                      <p>
+                                          <ul>
+                                          <li><a href="respuestademandado1.php">- Remisión a la justicia del tribunal y conformidad del demandado con los capitulos de nulidad invocados por la parte actora.</a></li>
+                                          <li><a href="respuestademandado2.php">- Comparecencia del demandado y designación de procurador y abogado</a></li>
+                                          <li><a href="respuestademandado3.php">- Escrito del demandado presentando, dentro del plazo concedido, oposición a la demanda y formulando a la vez acción reconvencional.</a></li>
+                                          <li><a href="respuestademandado4.php">- Decreto sobre admisión o no de la acción reconvencional.</a></li>
+                                          <li><a href="respuestademandado5.php">- Cuando el demandado se niega a recibir la citación, antes de procedes a declararle ausente en juicio al demandado, puede darse otro decreto de un tenor.</a></li>
+                                          <li><a href="respuestademandado6.php">- En la citación al demandado, en segundo señalamiento, se puede hacer constar que, si no quisiera intervenir el juicio, puede remitirse a la justicia del tribunal.</a></li>
+                                          <li><a href="respuestademandado7.php">- En el caso en que el demandado, en segundo señalamiento, no quiera recibir la citación, procederá la declaración de ausencia en el juicio del demandado.</a></li>
+                                          <li><a href="respuestademandado8.php">- Cuando el que no comparece, para contestar a la demanda reconvencional o concordar el dubio, es el actor, se le ha de citar de nuevo; y, si ni la segunda vez comparece, se entenderá que renuncia a la instancia, a no ser que el demandado o el fiscal inste en posecución de la misma (art. 91).</a></li>
+                                          <li><a href="respuestademandado9.php">- Decreto teniendo por renunciada la instancia por parte del actor.</a></li>
+                                          <li><a href="respuestademandado10.php">- Decreto de contitución del tribunal.</a></li>
+                                          <li><a href="respuestademandado11.php">- Fijación de la formula de dudas.</a></li>
+                                          <li><a href="respuestademandado12.php">- Señalar declaraciones de los testigos de la parte actora.</a></li>
+                                          <li><a href="respuestademandado13.php">- Cedula de citación para testigos.</a></li>
+                                          </ul>
+                                      </p>
+                                  </section>
                           </div>
+                        </section>
                     </div>
                 </div>
             </div>
