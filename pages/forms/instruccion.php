e7fead76aebe1b9553e6f11f0b7d731796dd442e
Adriana Centellas <adrianacentellas98@gmail.com>
2017-11-14 12:42:26 -0400

    instruccion - pag principal

---
+++ pages/forms/instruccion.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Nulidad Matrimonial - Caso nuevo</title>
+    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="../../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="../../plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="../../plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Sweet Alert Css -->
+    <link href="../../plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="../../css/style.css" rel="stylesheet">
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="../../css/themes/all-themes.css" rel="stylesheet" />
+</head>
+
+<body class="theme-blue">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Porfavor espere...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <!-- Top Bar -->
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+                <a href="javascript:void(0);" class="bars"></a>
+                <a class="navbar-brand" href="../../index.php">SEMINARIO</a>
+            </div>
+        </div>
+    </nav>
+    <!-- #Top Bar -->
+    <section>
+        <!-- Left Sidebar -->
+        <aside id="leftsidebar" class="sidebar">
+            <!-- User Info -->
+            <div class="user-info">
+                <div class="image">
+                    <img src="../../images/user.png" width="48" height="48" alt="User" />
+                </div>
+                <div class="info-container">
+                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Roberto Boceta</div>
+                    <div class="email">roberto.boceta@gmail.com</div>
+                    <div class="btn-group user-helper-dropdown">
+                        <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+                        <ul class="dropdown-menu pull-right">
+                          <li><a href="../../pages/forms/ingreso.php">Cerrar Sesion<i class="material-icons">input</i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- #User Info -->
+            <!-- Menu -->
+            <div class="menu">
+                <ul class="list">
+                    <li>
+                        <a href="indagacion.php">
+                            <i class="material-icons">library_add</i>
+                            <span>Caso nuevo</span>
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="javascript:void(0);" class="menu-toggle">              <!-- a href="../../pages/bla.html"-->
+                            <i class="material-icons">attach_file</i>
+                            <span>Dar seguimiento a caso</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li >
+                                <a href="indagacion.php">Indagación previa</a>
+                            </li>
+                            <li >
+                                <a href="introduccion.php">Fase Introductoria</a>
+                            </li>
+                            <li class="active">
+                                <a href="instruccion.php">Fase Instructoria</a>
+                            </li>
+                            <li>
+                                <a href="alegatoria.php">Fase Alegatoria</a>
+                            </li>
+                            <li>
+                                <a href="sentencia.php">Fase Decisoria</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="buscador.php">
+                            <i class="material-icons">search</i>
+                            <span>Buscar caso</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="nuevo_encargado.php">
+                            <i class="material-icons">account_box</i>
+                            <span>Agregar nuevo encargado</span>
+                        </a>
+        </aside>
+        <!-- #END# Left Sidebar -->
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                      <div class="header">
+                          <h2>CUESTIONARIOS PARA LA FASE DE INSTRUCCIÓN</h2>
+                      </div>
+                      <div class="body">
+                          <div id="wizard_vertical">
+                              <h2>Cuestionario del demandante</h2>
+                              <section>
+                                  <p>
+                                      <a href="cuestionariodemandante.php">Preguntas divididas en diferentes secciones para recopilar información de parte del demandante.</a>
+                                  </p>
+                              </section>
+                              <h2>Cuestionario del demandado</h2>
+                              <section>
+                                  <p>
+                                    <a href="cuestionariodemandado.php">Preguntas divididas en diferentes secciones para recopilar información de parte del demandado.</a>
+                                  </p>
+                              </section>
+                              <h2>Cuestionario de los testigos</h2>
+                              <section>
+                                  <p>
+                                    <a href="cuestionariotestigos.php">Preguntas divididas en diferentes secciones para recopilar información de parte externa a los exposos.</a>
+                                  </p>
+                                </section>
+                          </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <script src="../../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="../../plugins/jquery-validation/jquery.validate.js"></script>
+    <script src="../../plugins/jquery-steps/jquery.steps.js"></script>
+    <script src="../../plugins/sweetalert/sweetalert.min.js"></script>
+    <script src="../../plugins/node-waves/waves.js"></script>
+    <script src="../../js/admin.js"></script>
+    <script src="../../js/pages/forms/form-wizard.js"></script>
+    <script src="../../js/demo.js"></script>
+</body>
+</html>
