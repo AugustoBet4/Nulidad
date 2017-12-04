c824964bbacf4df56e295c4830505543e35a981b
PrimeAndrew <raparicio71@gmail.com>
2017-12-04 18:08:17 -0400

    Listo dashboard

--- js/pages/charts/morris.js
+++ js/pages/charts/morris.js
@@ -146,18 +146,19 @@ function getMorris(type, element) {
     } else if (type === 'donut') {
         Morris.Donut({
             element: element,
-            data: [{
-                label: 'Jam',
+            data: [
+              {
+                label: 'Prime',
                 value: 25
             }, {
-                    label: 'Frosted',
-                    value: 40
-                }, {
-                    label: 'Custard',
+                    label: 'Rodri',
                     value: 25
                 }, {
-                    label: 'Sugar',
-                    value: 10
+                    label: 'Adri',
+                    value: 25
+                }, {
+                    label: 'Guto',
+                    value: 25
                 }],
             colors: ['rgb(233, 30, 99)', 'rgb(0, 188, 212)', 'rgb(255, 152, 0)', 'rgb(0, 150, 136)'],
             formatter: function (y) {
